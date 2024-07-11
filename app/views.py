5fe6536c93ad686123137603088a170653974ec5
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 17:30:53 +0500

    :sparkles: show the list of products at home page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Product
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    products = Product.objects.all()
+    return render(request, 'index.html', {"products": products})
 
 
 def product(request, product_id):
