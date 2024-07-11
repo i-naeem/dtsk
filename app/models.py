569dd4c1ea1c26ec955077399c50588c1de018c9
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 16:57:06 +0500

    :recycle: refactor models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,13 @@ class Product(models.Model):
     quantity = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+
 
 class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="products/")
+
+    def __str__(self):
+        return self.image.name
