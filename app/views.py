cbf613289a5f5f838120f2ded030e8c3f2a47a58
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 17:11:55 +0500

    :sparkles: created `product` view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'detail.html', {"product": product})
