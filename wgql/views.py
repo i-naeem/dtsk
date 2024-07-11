8aa607195e27722b2a6d9ab229ae8daa259cd81d
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 09:58:44 +0500

    :sparkles: add the logic to add product

--- wgql/views.py
+++ wgql/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from wgql import forms
+from wgql import models
 
 
 def index(request):
+
     return render(request, 'index.html', {})
 
 
 def create_product(request):
-    return render(request, 'create_product.html', {})
+    context = {"product_form": forms.ProductForm()}
+
+    if request.method == "POST":
+        pf = forms.ProductForm(request.POST)
+        images = request.FILES.getlist("image_url")
+
+        if pf.is_valid():
+            p = pf.save()
+            for img in images:
+                img = models.ProductImages(image_url=img, product=p)
+                img.save()
+
+            return redirect(reverse('index'))
+
+    return render(request, "create_product.html", context)
 
 
 def create_images(request):
