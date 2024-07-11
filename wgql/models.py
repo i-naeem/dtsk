5b79fcf1cdea582dac40ed7c22d15e8e3397a1c2
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 08:02:10 +0500

    :card_file_box: allowed the order field to be empty

--- wgql/models.py
+++ wgql/models.py
@@ -7,20 +7,20 @@ class Order(models.Model):
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=255, help_text="Enter the name of the product")
-    product_quantity = models.PositiveIntegerField( help_text="Enter the quantity of the product")
-    product_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter the price of the product")
-    
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_name = models.CharField(
+        max_length=255, help_text="Enter the name of the product")
+    product_quantity = models.PositiveIntegerField(
+        help_text="Enter the quantity of the product")
+    product_price = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="Enter the price of the product")
+
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class ProductImages(models.Model):
     image_id = models.AutoField(primary_key=True)
-    image_url = models.ImageField(upload_to='product_images/', help_text="Select the product images")
-    
+    image_url = models.ImageField(
+        upload_to='product_images/', help_text="Select the product images")
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-
-    
-
-
