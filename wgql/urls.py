e9e22b4b01d8860d42987725de2cef692b830ce1
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 08:22:05 +0500

    :sparkles: created basic `templates`

--- wgql/urls.py
+++ wgql/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
+    path("create/product", views.create_product, name="create_product"),
+    path("create/images", views.create_images, name="create_images"),
+    path("create/order", views.create_order, name="create_order"),
 ]
--- wgql/views.py
+++ wgql/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Without GraphQL App")
+    return render(request, 'index.html', {})
+
+
+def create_product(request):
+    return render(request, 'create_product.html', {})
+
+
+def create_images(request):
+    return render(request, 'create_images.html', {})
+
+
+def create_order(request):
+    return render(request, 'create_order.html', {})
