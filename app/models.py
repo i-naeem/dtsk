f0180eed55d8af7ecd74c7c76544b06affe3d51b
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 16:50:40 +0500

    :card_file_box: created `Image` & `Product` model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="products/")
