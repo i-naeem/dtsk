eff32de94818a0bb8b26a1a8eb6fb4d49b4bfa28
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 10:31:57 +0500

    :sparkles: created `orderform`

--- wgql/forms.py
+++ wgql/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from wgql.models import Product, ProductImages
+from wgql.models import Product, Order
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["product_name", "product_quantity", "product_price"]
+
+
+class OrderForm(forms.ModelForm):
+    products = forms.ModelMultipleChoiceField(
+        queryset=Product.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        help_text="Select the products for this order"
+    )
+
+    class Meta:
+        model = Order
+        fields = ["products"]
