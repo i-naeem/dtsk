9cae0cf189a3406e360fe41275ad564b3f7b4636
Muhammad Naeem <inaeem@protonmail.com>
2024-07-11 07:02:07 +0500

    :card_file_box: created `Order`, `Product` and `ProductImages` models

--- wgql/models.py
+++ wgql/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_date = models.DateField(auto_now_add=True)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255, help_text="Enter the name of the product")
+    product_quantity = models.PositiveIntegerField( help_text="Enter the quantity of the product")
+    product_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter the price of the product")
+    
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+class ProductImages(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    image_url = models.ImageField(upload_to='product_images/', help_text="Select the product images")
+    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+    
+
+
